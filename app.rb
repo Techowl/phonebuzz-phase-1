affb997716f3f19525dc8a0d2fc31ec5ed4f9d80
Daniel Kimbel <dkkimbel@gmail.com>
2014-07-19 22:27:36 -0700

    Change way auth token is stored

--- app.rb
+++ app.rb
@@ -4,10 +4,11 @@ require 'sinatra'
 require 'dotenv'
 
 Dotenv.load
+@auth_token = ENV['AUTH_TOKEN']
 
 helpers do
-  def request_valid
-    validator = Twilio::Util::RequestValidator.new(ENV['AUTH_TOKEN'])
+  def request_valid?
+    validator = Twilio::Util::RequestValidator.new(@auth_token)
     uri = request.url
     params = env['rack.request.query_hash']
     signature = env['HTTP_X_TWILIO_SIGNATURE']
@@ -16,7 +17,7 @@ helpers do
 end
 
 get '/hello' do
-  return unless request_valid
+  return unless request_valid?
   Twilio::TwiML::Response.new do |r|
     r.Gather :finishOnKey => '#', :action => '/hello/fizzbuzz', :method => 'get' do |g|
       g.Say 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 999 followed by the pound sign.'
@@ -25,7 +26,7 @@ get '/hello' do
 end
 
 get '/hello/fizzbuzz' do
-  return unless request_valid
+  return unless request_valid?
   number = params['Digits'].to_i
   redirect '/hello' unless (number >= 1 && number <= 999)
   Twilio::TwiML::Response.new do |r|
