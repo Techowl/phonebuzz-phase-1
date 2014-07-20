bb28c932ecea18fb9ea353330727defb5d1b2875
Daniel Kimbel <dkkimbel@gmail.com>
2014-07-19 20:45:35 -0700

    Experiment with taking input

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ require 'sinatra'
 
 get '/hello' do
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello Monkey'
+    r.Gather :numDigits => '2', :action => '/hello/fizzbuzz', :method => 'get' do |g|
+      g.Say :voice => 'woman', 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 99.'
+    end
+  end.text
+end
+
+get '/hello/fizzbuzz' do
+  redirect '/hello' unless params['Digits'] == '1'
+  Twilio::TwiML::Response.new do |r|
+    r.Say :voice => 'woman', 'Congratulations, you\'ve reached the FizzBuzz stage!'
   end.text
 end
