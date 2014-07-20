f0c9d661f69d6ae7af9206e89fadfdff3530d8d2
Daniel Kimbel <dkkimbel@gmail.com>
2014-07-19 20:54:36 -0700

    Capture a number up to 999 and repeat it to the user

--- app.rb
+++ app.rb
@@ -4,15 +4,16 @@ require 'sinatra'
 
 get '/hello' do
   Twilio::TwiML::Response.new do |r|
-    r.Gather :numDigits => '2', :action => '/hello/fizzbuzz', :method => 'get' do |g|
-      g.Say 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 99.'
+    r.Gather :finishOnKey => '#', :numDigits => 3, :action => '/hello/fizzbuzz', :method => 'get' do |g|
+      g.Say 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 999 followed by the pound sign.'
     end
   end.text
 end
 
 get '/hello/fizzbuzz' do
-  redirect '/hello' unless params['Digits'] == '1'
+  number = params['Digits']
+  # redirect '/hello' unless number == '1'
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Congratulations, you\'ve reached the FizzBuzz stage!'
+    r.Say "Congratulations, you've reached the FizzBuzz stage with number #{number}!"
   end.text
 end
