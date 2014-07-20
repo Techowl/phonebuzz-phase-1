2e3b549985cd17eb67d490292b39fc33056dcbfb
Daniel Kimbel <dkkimbel@gmail.com>
2014-07-19 21:04:43 -0700

    Fix illegal function that broke code while checking for type of user's entered number

--- app.rb
+++ app.rb
@@ -5,14 +5,14 @@ require 'sinatra'
 get '/hello' do
   Twilio::TwiML::Response.new do |r|
     r.Gather :finishOnKey => '#', :action => '/hello/fizzbuzz', :method => 'get' do |g|
-      g.Say 'To receive your FizzBuzz results, please enter a number between 1 and 999 followed by the pound sign.'
+      g.Say 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 999 followed by the pound sign.'
     end
   end.text
 end
 
 get '/hello/fizzbuzz' do
   number = params['Digits']
-  redirect '/hello' unless (type(number) == 'int' && number >= 1 && number <= 999)
+  redirect '/hello' unless (number.is_a?(Fixnum) && number >= 1 && number <= 999)
   Twilio::TwiML::Response.new do |r|
     r.Say "Congratulations, you've reached the FizzBuzz stage with number #{number}!"
   end.text
