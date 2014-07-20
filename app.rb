74f8a27573f3ba3ba5fb3bc982980f713505b085
Daniel Kimbel <dkkimbel@gmail.com>
2014-07-19 20:47:49 -0700

    Debug by removing voice parameter for 'say'

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra'
 get '/hello' do
   Twilio::TwiML::Response.new do |r|
     r.Gather :numDigits => '2', :action => '/hello/fizzbuzz', :method => 'get' do |g|
-      g.Say :voice => 'woman', 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 99.'
+      g.Say 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 99.'
     end
   end.text
 end
@@ -13,6 +13,6 @@ end
 get '/hello/fizzbuzz' do
   redirect '/hello' unless params['Digits'] == '1'
   Twilio::TwiML::Response.new do |r|
-    r.Say :voice => 'woman', 'Congratulations, you\'ve reached the FizzBuzz stage!'
+    r.Say 'Congratulations, you\'ve reached the FizzBuzz stage!'
   end.text
 end
