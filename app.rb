34e93e2e78b3ea0c90ed07269d96760621e032ca
Daniel Kimbel <dkkimbel@gmail.com>
2014-07-19 21:00:51 -0700

    Redirect user unless a valid number is entered

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ require 'sinatra'
 
 get '/hello' do
   Twilio::TwiML::Response.new do |r|
-    r.Gather :finishOnKey => '#', :numDigits => 3, :action => '/hello/fizzbuzz', :method => 'get' do |g|
-      g.Say 'Hello! To receive your FizzBuzz results, please enter a number between 1 and 999 followed by the pound sign.'
+    r.Gather :finishOnKey => '#', :action => '/hello/fizzbuzz', :method => 'get' do |g|
+      g.Say 'To receive your FizzBuzz results, please enter a number between 1 and 999 followed by the pound sign.'
     end
   end.text
 end
 
 get '/hello/fizzbuzz' do
   number = params['Digits']
-  # redirect '/hello' unless number == '1'
+  redirect '/hello' unless (type(number) == 'int' && number >= 1 && number <= 999)
   Twilio::TwiML::Response.new do |r|
     r.Say "Congratulations, you've reached the FizzBuzz stage with number #{number}!"
   end.text
